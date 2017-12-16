027fd15adb0ecf6f2ce40bf8fbae278895f5e675
imsenthur <imsenthur@gmail.com>
2017-12-17 01:47:54 +0530

    Preprocessed and Inputs applied

--- preprocessingframes.py
+++ preprocessingframes.py
@@ -2,11 +2,25 @@ import numpy as np
 import cv2
 from PIL import ImageGrab
 import pyautogui
+import time
+from Input import PressKey, ReleaseKey, W, A, S, D
 
 def preprocess_image(orig_img):
     processed_img = cv2.cvtColor(orig_img, cv2.COLOR_BGR2GRAY)
     processed_img = cv2.Canny(processed_img, threshold1 = 200, threshold2 = 300)
     return processed_img
+
+for i in list(range(4))[::-1]:
+    print(i+1)
+    time.sleep(i)
+
+PressKey(W)
+print('Accelerating')
+time.sleep(3)
+ReleaseKey(W)
+print('Decelerating')
+
+
 
 while True:
     capture = np.array(ImageGrab.grab(bbox = (0,40,800,640)))
