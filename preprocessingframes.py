0d4e6eadcacf3f855cdfce5991f80af594d1bef0
imsenthur <imsenthur@gmail.com>
2017-12-17 10:06:31 +0530

    Preprocessing Done

--- preprocessingframes.py
+++ preprocessingframes.py
@@ -5,22 +5,31 @@ import pyautogui
 import time
 from Input import PressKey, ReleaseKey, W, A, S, D
 
+def draw_lines(img, lines):
+    try:
+        for line in lines:
+            coords = line[0]
+            cv2.line(img, (coords[0],coords[1]), (coords[2],coords[3]), [255,255,255], 3)
+    except:
+        pass
+
+def roi(img, vertices):
+    mask = np.zeros_like(img)
+    cv2.fillPoly(mask, vertices, 255)
+    masked = cv2.bitwise_and(img, mask)
+    return masked
+
 def preprocess_image(orig_img):
     processed_img = cv2.cvtColor(orig_img, cv2.COLOR_BGR2GRAY)
     processed_img = cv2.Canny(processed_img, threshold1 = 200, threshold2 = 300)
+    processed_img = cv2.GaussianBlur(processed_img, (5,5), 0)
+    
+    vertices = np.array([[10,550], [10,325], [300,325], [500,325], [800,325], [800,550]])
+    processed_img = roi(processed_img, [vertices])
+
+    lines = cv2.HoughLinesP(processed_img, 1, np.pi/180, 180, np.array([]), 100, 5)
+    draw_lines(processed_img, lines)
     return processed_img
-
-for i in list(range(4))[::-1]:
-    print(i+1)
-    time.sleep(i)
-
-PressKey(W)
-print('Accelerating')
-time.sleep(3)
-ReleaseKey(W)
-print('Decelerating')
-
-
 
 while True:
     capture = np.array(ImageGrab.grab(bbox = (0,40,800,640)))
